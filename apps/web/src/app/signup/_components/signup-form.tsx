36d69b9446711945577c0969d9d986402ba45e99
JustinRoderick <justinroderick258@gmail.com>
2025-10-29 23:47:30 -0400

    google oauth works

--- apps/web/src/app/signup/_components/signup-form.tsx
+++ apps/web/src/app/signup/_components/signup-form.tsx
@@ -20,12 +20,19 @@ import {
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
-const formSchema = z.object({
-  name: z.string().min(2, "Name must be at least 2 characters"),
-  email: z.email("Invalid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-  confirmPassword: z.string().min(8, "Password must be at least 8 characters"),
-});
+const formSchema = z
+  .object({
+    name: z.string().min(2, "Name must be at least 2 characters"),
+    email: z.email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 type FormSchema = z.infer<typeof formSchema>;
 
@@ -189,11 +196,35 @@ export default function SignupForm() {
           }}
         />
         <Field>
-          <Button type="submit">Create Account</Button>
+          <Button
+            type="submit"
+            disabled={!form.state.canSubmit || form.state.isSubmitting}
+          >
+            {form.state.isSubmitting ? "Creating account..." : "Create Account"}
+          </Button>
         </Field>
         <FieldSeparator>Or continue with</FieldSeparator>
         <Field>
-          <Button variant="outline" type="button">
+          <Button
+            variant="outline"
+            type="button"
+            onClick={async () => {
+              await authClient.signIn.social(
+                {
+                  provider: "google",
+                },
+                {
+                  onSuccess: () => {
+                    router.push("/dashboard");
+                    toast.success("Sign Up successful");
+                  },
+                  onError: (error) => {
+                    toast.error(error.error.message || error.error.statusText);
+                  },
+                }
+              );
+            }}
+          >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path
                 d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
